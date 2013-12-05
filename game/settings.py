eca40d39532cbef794dc398dbf076122f100b02d
vaaction <ibukanov87@gmail.com>
2013-12-06 03:00:50 +0400

    add heroku

--- game/settings.py
+++ game/settings.py
@@ -12,13 +12,7 @@ MANAGERS = ADMINS
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'sqlite3',                      # Or path to database file if using sqlite3.
-    }
-}
+DATABASES = {'default': dj_database_url.config()}
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
