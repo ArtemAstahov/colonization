eadb0323453f812a23c677420e27e3b5c85458e3
vaaction <ibukanov87@gmail.com>
2014-01-04 16:26:26 +0300

    change project structure

--- game/urls.py
+++ game/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from game import views
 
 urlpatterns = patterns('',
-    url(r'^game', views.game),
+    url(r'^$', views.game),
     url(r'^create_game', views.create_game),
     url(r'^join_to_game', views.join_to_game),
 )
