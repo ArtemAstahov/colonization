243093edeeccf1e1281ee730cd59674a88a3a7f1
Ilya <ibukanov87@gmail.com>
2014-01-22 21:57:09 +0400

    vk

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, url, include
+from main import views
 
 
 urlpatterns = patterns('',
-    url(r'^$', include('social_auth.urls')),
+    url(r'', include('social_auth.urls')),
+    url(r'^$', views.home),
     url(r'^game/', include('game.urls')),
     url(r'^ajax/', include('ajax.urls')),
 )
