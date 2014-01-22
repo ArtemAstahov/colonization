c58cb0c4c0bbc09b4f73745212ea5996fa219fd8
Ilya <ibukanov87@gmail.com>
2014-01-23 01:45:30 +0400

    vk

--- main/settings.py
+++ main/settings.py
@@ -60,7 +60,6 @@ MEDIA_URL = ''
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
 import os
-STATIC_ROOT = 'static'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
