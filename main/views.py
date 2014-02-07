ae127461e01e1f7620cf7431194511da9b7a02dc
Ilya <ibukanov87@gmail.com>
2014-02-07 11:08:31 +0400

    update home page

--- main/views.py
+++ main/views.py
@@ -8,9 +8,10 @@ from game.models import get_active_game, get_host_games, get_player, Game
 
 
 class UserScores:
-    def __init__(self, name, score):
+    def __init__(self, name, score, is_current_user):
         self.name = name
         self.score = score
+        self.is_current_user = is_current_user
 
 
 def home(request):
@@ -18,15 +19,24 @@ def home(request):
     users = User.objects.all()
     for user in users:
         score = get_user_score(user)
-        user_scores.append(UserScores(name=user.first_name + " " + user.last_name, score=score))
+        if request.user == user:
+            user_scores.append(UserScores(name=user.first_name + " " + user.last_name,
+                                          score=score, is_current_user=True))
+        else:
+            user_scores.append(UserScores(name=user.first_name + " " + user.last_name,
+                                          score=score, is_current_user=False))
+    user_scores.sort(key=operator.attrgetter('score'))
+    user_scores.sort(key=operator.attrgetter('is_current_user'))
+    user_scores.reverse()
+    user_scores = user_scores[0:5]
     user_scores.sort(key=operator.attrgetter('score'))
     user_scores.reverse()
 
     if request.user.is_authenticated() and get_player(request.user) is not None and get_active_game(request.user):
         return render(request, 'home.html',
-                      {'game': get_active_game(request.user), 'user_scores': user_scores[0:5]})
+                      {'game': get_active_game(request.user), 'user_scores': user_scores})
     else:
-        return render(request, 'home.html', {'host_games': get_host_games(), 'user_scores': user_scores[0:5]})
+        return render(request, 'home.html', {'host_games': get_host_games(), 'user_scores': user_scores})
 
 
 @login_required
---
+++ static/js/main/home.js
@@ -0,0 +1,11 @@
+$("#createGame").click(function(){
+    window.location.replace("/game/create_game")
+});
+
+$("#toGame").click(function(){
+    window.location.replace("/game")
+});
+
+$("#refreshHosts").click(function(){
+    window.location.replace("/")
+});
